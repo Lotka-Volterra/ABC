0e771f99395a70ec1b9e3009ce22ec043fabf0d4
sk_vaio <84185140+Lotka-Volterra@users.noreply.github.com>
2023-09-17 18:29:32 +0900

    001-025整理

--- ABC001-025/ABC011/ABC011_B.py
+++
@@ -1,4 +0,0 @@
-s = input()
-low = s[1:].lower()
-up = s[0].upper()
-print(up+low)
--- ABC001-025/ABC017/ABC017_A.py
+++
@@ -1,5 +0,0 @@
-point = 0
-for i in range(3):
-    j, k = map(int, input().split())
-    point += int(j*k*0.1)
-print(point)
