bdbda3619afd4047b5b54e4faea3afa67e3ce5c1
sk_vaio <84185140+Lotka-Volterra@users.noreply.github.com>
2023-11-05 20:59:10 +0900

    今日の精進3

--- Typical/10の位の求め方.py
+++ Typical/10の位の求め方.py
@@ -7,3 +7,15 @@ n % 10
 # floor(n/10)mod10らしい
 # 100の位
 (n // 100) % 10
+
+
+# n進法、n進数表記
+# integerをbase進数表記の文字列にして返す関数
+# https://atcoder.jp/contests/abc186/editorial/403
+def toNBase(integer, base):
+    answerString = ""
+    # integerが0より大きい間
+    while integer:
+        answerString = str(integer % n) + answerString
+        integer //= base
+    return integer
