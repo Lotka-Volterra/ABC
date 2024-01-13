cc61549afe6b6c56a18074e777ade3523823cd49
sk_vaio <84185140+Lotka-Volterra@users.noreply.github.com>
2024-01-14 05:43:29 +0900

    ファイル移動、最近の精進

--- 典型テクニック/二部探索.py
+++ 典型テクニック/二部探索.py
@@ -3,20 +3,20 @@
 n, x = map(int, input().split())
 a = list(map(int, input().split()))
 # startindexは探索範囲の始端、endindexは探索範囲の末端
-startindex = 0
-endindex = n-1
+start_index = 0
+end_index = n - 1
 # 探索範囲が存在する間、比較を続ける
-while startindex <= endindex:
+while start_index <= end_index:
     # midindexは探索範囲の中央のインデックス、midAはその値
-    midindex = (endindex+startindex)//2
-    midA = a[midindex]
+    mid_index = (end_index + start_index) // 2
+    midA = a[mid_index]
     # 見つかったら何項目かを出力
     if x == midA:
-        print(midindex+1)
+        print(mid_index + 1)
         quit()
     # xがmidAより小さい場合は、探索範囲を末端側から狭める
     elif x < midA:
-        endindex = midindex-1
+        end_index = mid_index - 1
     # xがmidAより大きい場合は、探索範囲を始端側から狭める
     else:
-        startindex = midindex+1
+        start_index = mid_index + 1
